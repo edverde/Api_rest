23f7c018aa5438df6eb738e55983db12946dcc85
edverde <eduardvr83@gmail.com>
2022-07-18 11:00:17 +0200

    first commit

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+        
+        $validatedData['passsword'] = Hash::make($request->password);
+        
+        $user = User::create($validatedData);
+       
+        $accessToken = $user->createToken('authToken')->accessToken;
+        
+        return response([
+            
+            'user' => $user,
+            'acess_token' => $accessToken
+        ]);
+        
+    }
+}
