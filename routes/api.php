60b17348d977d420159125b20d8a5cdb3cb9f328
edverde <eduardvr83@gmail.com>
2022-08-23 13:51:50 +0200

    use rectificados

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\CEOController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
 
 /*
 |--------------------------------------------------------------------------
