ad44e7c8294548e24cd195fe1266e06129349994
edverde <eduardvr83@gmail.com>
2022-08-25 11:37:42 +0200

    funcion index creada

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Models\Game;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
@@ -17,7 +19,15 @@ class UserController extends Controller
      */
     public function index()
     {
-       
-    }       
+        $users = DB::table('users')
+        ->select('*')
+        ->get();
+    
+        return response()->json(['users' => $users,'status' => 200]);
+    }   
+}
+
+      
         
-    }
+
+
